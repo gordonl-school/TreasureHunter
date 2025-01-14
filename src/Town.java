14fa94eed91ebc4262971bc72a4df0af986aef17
BT_1N3_03 <sample@bths.edu>
2025-01-14 13:54:20 -0500

    completed TH-7 (adding dig for gold)

--- src/Town.java
+++ src/Town.java
@@ -11,6 +11,7 @@ public class Town {
     private Terrain terrain;
     private String printMessage;
     private boolean toughTown;
+    private boolean dug;
 
     /**
      * The Town Constructor takes in a shop and the surrounding terrain, but leaves the hunter as null until one arrives.
@@ -29,6 +30,7 @@ public class Town {
 
         // higher toughness = more likely to be a tough town
         toughTown = (Math.random() < toughness);
+        dug = false;
     }
 
     public Terrain getTerrain() {
@@ -109,6 +111,27 @@ public class Town {
                 printMessage += Colors.RED + "That'll teach you to go lookin' fer trouble in MY town! Now pay up!";
                 printMessage += "\nYou lost the brawl and pay " + Colors.YELLOW + goldDiff + " gold." + Colors.RESET;
                 hunter.changeGold(-goldDiff);
+            }
+        }
+    }
+
+    public void digForGold() {
+        if (dug == true) {
+            printMessage = "You already dug for gold in this town.\n";
+        } else {
+            if (hunter.hasItemInKit("shovel")) {
+                int chance = (int) (Math.random() * 2) + 1;
+                if (chance == 1) {
+                    int amountReceived = (int) (Math.random() * 19) + 1;
+                    hunter.changeGold(amountReceived);
+                    printMessage = "You dug up " + amountReceived + " gold!\n";
+                    dug = true;
+                } else {
+                    printMessage = "You dug but only found dirt.\n";
+                    dug = true;
+                }
+            } else {
+                printMessage = "You can't dig for gold without a shovel.\n";
             }
         }
     }
